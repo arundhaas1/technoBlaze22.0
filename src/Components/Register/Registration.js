1059fcbf368f3827b6df664e3085e5e4b84a6432
arundhaas1 <arunram.9215@gmail.com>
2022-04-13 20:22:00 +0530

    add

--- src/Components/Register/Registration.js
+++ src/Components/Register/Registration.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import "./Registration.css";
 import { initializeApp } from "firebase/app";
-import { collection,addDoc } from "firebase/firestore/lite";
+import { collection, addDoc } from "firebase/firestore/lite";
 import { getFirestore } from "firebase/firestore/lite";
 
 function Registration() {
-
   const [Name, setName] = useState("");
   const [Email, setEmail] = useState("");
   const [Phone, setPhone] = useState("");
@@ -13,6 +12,7 @@ function Registration() {
   const [Department, setDepartment] = useState("");
   const [Year, setYear] = useState("");
   const [Event, setEvent] = useState("");
+  const [attachment, setAttachment] = useState("");
 
   const firebaseConfig = {
     apiKey: "AIzaSyCvh298NyoIYRVUXv9OsFsJXm6BxEt67ak",
@@ -24,37 +24,65 @@ function Registration() {
     measurementId: "G-BQPEX3VT9J",
   };
 
-  const app = initializeApp(firebaseConfig);
-  const db=getFirestore(app);
+  const toBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
 
-  const postIt=()=>{
-   if(Email.includes("@gmail.com")){
-    if(Name!=="" && Email!=="" &&Phone!=="" &&College!=="" &&Department!=="" &&Year!=="" &&Event!=="" ){
-      sendIt();
-    }else{
-        alert("Some datas are missing")
+  const onFileChange = async () => {
+    var fileInput = document.querySelector("#input");
+    if (fileInput) {
+      var file = fileInput.files[0];
+      const base64String = await toBase64(file);
+      setAttachment(base64String);
     }
-   }else{
-     alert("Enter valid email id")
-   }
-  }
+  };
 
-  const sendIt=async()=>{
-    await addDoc(collection(db,"technoblaze"),{
-      college:College,
-      department:Department,
-      email:Email,
-      events:Event,
-      name:Name,
-      phone:Phone,
-      year:Year
-    }).then(function(res){
-      alert("Data is sucessfully added")
-    }).catch(function(err){
-      console.log(err)
-      alert("Data can not be stored")
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const postIt = () => {
+    if (Email.includes("@gmail.com")) {
+      if (
+        Name !== "" &&
+        Email !== "" &&
+        Phone !== "" &&
+        College !== "" &&
+        Department !== "" &&
+        Year !== "" &&
+        Event !== ""
+      ) {
+        sendIt();
+      } else {
+        alert("Some datas are missing");
+      }
+    } else {
+      alert("Enter valid email id");
+    }
+  };
+
+  const sendIt = async () => {
+    await addDoc(collection(db, "technoblaze"), {
+      college: College,
+      department: Department,
+      email: Email,
+      events: Event,
+      name: Name,
+      phone: Phone,
+      year: Year,
+      attachment: attachment,
     })
-  }
+      .then(function (res) {
+        alert("Data is sucessfully added");
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert("Data can not be stored");
+      });
+  };
 
   return (
     <div className="registration">
@@ -107,10 +135,33 @@ function Registration() {
               <input name="accommodation" type="radio" />
               <label>No</label>
             </div>
+            <div className="addpayment">
+              <div className="addp">
+                <p>Add your Online Payment ScreenShot here..</p>
+              </div>
+              <label className="addimage">
+                <i>{attachment ? "Payment Added" : "Add Payment"}</i>
+                <input
+                  onChange={onFileChange}
+                  id="input"
+                  style={{ display: "none" }}
+                  type="file"
+                  accept=".jpg,.jpeg,.png"
+                />
+              </label>
+            </div>
           </div>
           <h4 onClick={postIt}>Submit</h4>
         </div>
-        <p className="pay__amount" style={{color:"white"}}>Spot Payment : <span> 250rs</span></p>
+        <div className="pay">
+          <p className="pay__amount" style={{ color: "white" }}>
+            Spot Payment : <span> 250rs</span>
+          </p>
+          <p className="or">OR</p>
+          <p className="gpay">
+            GooglePay,Paytm,PhonePe : <span>+91 6374006612</span>
+          </p>
+        </div>
       </div>
     </div>
   );
